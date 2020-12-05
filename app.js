4a9abc9e4db813aa100d55e361d82a8180eceb28
Wakidur Rahman <wakidur@gmail.com>
2020-12-06 01:03:48 +0600

    add new link

--- app.js
+++ app.js
@@ -87,17 +87,9 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/book', bookRouter);
 app.use('/api/v1/requetbook', requetForBookRouter);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+app.get('/', (req, res) => {
+  res.send('API is running....');
+});
 app.all('*', (req, res, next) => {
   next(new ErrorResponse(`Can't find ${req.originalUrl} on this server!`, 404));
 });
